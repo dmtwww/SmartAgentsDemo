9404933f6cb0170fe7d5e4e65f3a5bb3a6e9f02d
Remco Brosky <remco.brosky@microsoft.com>
2025-02-05 17:23:24 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using AutoGen.Core;
+using DevTeamDemo.Resources;
+
+Console.Clear();
+Console.WriteLine("--- CREATE RESOURCES ---");
+Console.WriteLine("- Read Configuration");
+var resourceCreator = new ResourceCreator();
+Console.WriteLine("- Create Azure OpenAI Client");
+var openAIClient = resourceCreator.CreateOpenAIClient();
+Console.WriteLine("- Create Code Execution environment");
+var codingKernel = resourceCreator.CreateCodingKernel();
+
+
+Console.WriteLine("- Create Coding Agent");
+var codingAssistant = resourceCreator.CreateOpenAIChatAgent(openAIClient, "coder", "you can write code to resolve a task");
+Console.WriteLine("- Create Groupchat Admin");
+var groupChatAdmin = resourceCreator.CreateOpenAIChatAgent(openAIClient, "groupadmin", "you manage the groupchat");
+Console.WriteLine("- Create Code Executor Agent");
+var codeExecutor = resourceCreator.CreateCodeExecutorAgent(codingKernel);
+Console.WriteLine("- Create User Proxy Agent");
+var userProxy = resourceCreator.CreateUserProxyAgent();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("All done and ready, what do you want me to do?");
+var task = Console.ReadLine();
+
+if (task.ToLower().Contains("exit"))
+{
+    Console.WriteLine("Goodbye!");
+    Console.ForegroundColor = ConsoleColor.Gray;
+    return;
+}
+
+var groupChat = new GroupChat(
+    admin: groupChatAdmin,
+    members: [
+        codingAssistant, 
+        codeExecutor, 
+        userProxy
+    ]);
+
+var chatHistory = new List<IMessage>
+        {
+            new TextMessage(Role.User, task)
+            {
+                From = userProxy.Name
+            }
+        };
+
+await foreach (var message in groupChat.SendAsync(chatHistory, maxRound: 10))
+{
+    if (message.From == userProxy.Name && message.GetContent().Contains("[GROUPCHAT_TERMINATE]"))
+    {
+        // Task complete!
+        break;
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.Gray;
+
---
+++ Resources/ResourceCreator.cs
@@ -0,0 +1,135 @@
+﻿using AutoGen;
+using AutoGen.Core;
+using AutoGen.DotnetInteractive;
+using AutoGen.DotnetInteractive.Extension;
+using AutoGen.OpenAI;
+using AutoGen.OpenAI.Extension;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using dotenv.net;
+using Microsoft.DotNet.Interactive;
+using OpenAI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeamDemo.Resources;
+
+public class ResourceCreator
+{
+
+    private Uri _azureOpenAIEndpoint;
+    private string _azureOpenAIDeploymentName;
+
+    public ResourceCreator()
+    {
+        DotEnv.Load();
+        _azureOpenAIEndpoint = new Uri(Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT"));
+        _azureOpenAIDeploymentName = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME");
+
+    }
+
+    public OpenAIClient CreateOpenAIClient()
+    {
+        var openAIClient = new AzureOpenAIClient(
+            _azureOpenAIEndpoint,
+            new AzureCliCredential()
+            );
+        return openAIClient;
+    }
+
+    public CompositeKernel CreateCodingKernel()
+    {
+        var codingKernel = DotnetInteractiveKernelBuilder
+            .CreateEmptyInProcessKernelBuilder()
+            .AddCSharpKernel(aliases: ["dotnet"])
+            .AddPythonKernel(venv: "python3")
+            .Build();
+        return codingKernel;
+    }
+
+    public MiddlewareStreamingAgent<OpenAIChatAgent> CreateOpenAIChatAgent(OpenAIClient client, string agentName, string systemMessage)
+    {
+        var openAIChatAgent = new OpenAIChatAgent(
+            chatClient: client.GetChatClient(_azureOpenAIDeploymentName),
+            name: agentName,
+            systemMessage: systemMessage
+            )
+            .RegisterMessageConnector()
+            .RegisterPrintMessage();
+
+        return openAIChatAgent;
+    }
+
+    public MiddlewareAgent<UserProxyAgent> CreateUserProxyAgent()
+    {
+        var userProxyAgent = new UserProxyAgent(
+            name: "user",
+            humanInputMode: HumanInputMode.ALWAYS)
+            .RegisterPrintMessage();
+
+        return userProxyAgent;
+    }
+
+    public MiddlewareAgent<DefaultReplyAgent> CreateCodeExecutorAgent(CompositeKernel kernel)
+    {
+        var runner = new DefaultReplyAgent(
+        name: "runner",
+        defaultReply: "No code available, coder, write code please")
+        .RegisterMiddleware(async (msgs, option, agent, ct) =>
+        {
+            var mostRecentCoderMessage = msgs.LastOrDefault(x => x.From == "coder") ?? throw new Exception("No coder message found");
+
+            if (mostRecentCoderMessage.GetContent().Contains("```csharp"))
+            {
+
+                if (mostRecentCoderMessage.ExtractCodeBlock("```csharp", "```") is string code)
+                {
+                    var result = await kernel.RunSubmitCodeCommandAsync(code, "csharp2");
+                    // only keep the first 500 characters
+                    if (result.Length > 10000)
+                    {
+                        result = result.Substring(0, 10000);
+                    }
+                    result = $"""
+                    # [CODE_BLOCK_EXECUTION_RESULT]
+                    {result}
+                    """;
+
+                    return new TextMessage(Role.Assistant, result, from: agent.Name);
+                }
+            }
+
+            if (mostRecentCoderMessage.GetContent().Contains("```python"))
+            {
+
+                if (mostRecentCoderMessage.ExtractCodeBlock("```python", "```") is string code)
+                {
+                    var result = await kernel.RunSubmitCodeCommandAsync(code, "python");
+                    // only keep the first 500 characters
+                    if (result.Length > 10000)
+                    {
+                        result = result.Substring(0, 10000);
+                    }
+                    result = $"""
+                    # [CODE_BLOCK_EXECUTION_RESULT]
+                    {result}
+                    """;
+
+                    return new TextMessage(Role.Assistant, result, from: agent.Name);
+                }
+            }
+
+            return await agent.GenerateReplyAsync(msgs, option, ct);
+
+        })
+        .RegisterPrintMessage();
+        
+        return runner;
+
+    }  
+
+}
